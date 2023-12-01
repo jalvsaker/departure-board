b698dcfc736ae8675e95705557f6095419af469b
jalvsaker <jalvsaker@users.noreply.github.com>
2023-12-01 22:43:07 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,9 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   output: "hybrid",
   adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
 });
